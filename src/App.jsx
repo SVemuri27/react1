7e5607cadee32fbd20a22cf22df0e2c5dad2a448
SVemuri27 <sucharitha27102001@gmail.com>
2024-12-13 19:04:20 +0530

    nav2 commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import About from './pages/AboutPage';
 
-import Navbar from './components/NavbarElements';
+// import Navbar from './components/NavbarElements';
 
 
 
