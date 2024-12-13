096ee86ea85fac30be7cb2d3c1105199b8e958bb
SVemuri27 <sucharitha27102001@gmail.com>
2024-12-13 23:31:33 +0530

    nav8 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/navbar';
+import Nav from './components/Nav';
 import HomePage from './pages/HomePage';
-import About from './pages/AboutPage';
-// import './App.css';
+import About from './pages/About';
+import './App.css';
+import Nav from './components/Nav';
 
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
+      <Nav />
       <Routes>
         <Route path="/Home" element={<HomePage />} />
         <Route path="/About" element={<About />} />
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+// import { FaBars } from "react-icons/fa";
+// import { NavLink as Link } from "react-router-dom";
+
+// Nav.jsx
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav>
+      <h1>Navigation Bar</h1>
+    </nav>
+  );
+};
+
+export default Nav;
