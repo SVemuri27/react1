723490895aaa70110ea23867347eb8e370f7a24a
SVemuri27 <sucharitha27102001@gmail.com>
2024-12-13 19:07:40 +0530

    nav3 commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import Navbar from "./components/Navbar";
 import HomePage from './pages/HomePage';
