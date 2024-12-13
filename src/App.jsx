887fd6be2c3291f4f366a81bae2ef07a863b9b65
SVemuri27 <sucharitha27102001@gmail.com>
2024-12-13 22:32:01 +0530

    nav3 commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 // import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-
+// import './App.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
