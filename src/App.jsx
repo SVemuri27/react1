2eb772665a8807ea09484727702f8d9f4795dc03
SVemuri27 <sucharitha27102001@gmail.com>
2024-12-13 23:33:12 +0530

    nav8 commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import Nav from './components/Nav';
 import HomePage from './pages/HomePage';
-import About from './pages/About';
+import About from './pages/AboutPages';
 import './App.css';
 import Nav from './components/Nav';
 
