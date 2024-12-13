9f77ba9796c093bdbad7b1023fc9e45334bb21b8
SVemuri27 <sucharitha27102001@gmail.com>
2024-12-13 19:59:15 +0530

    nav1 commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/NavbarComponents';
+// import Navbar from './components/NavbarComponents';
 import HomePage from './pages/HomePage';
 import About from './pages/AboutPage';
 
