f72af0f1b1e93226380ae734cfa18a9b6f9a1c52
SVemuri27 <sucharitha27102001@gmail.com>
2024-12-13 22:47:05 +0530

    nav5 commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import HomePage from './pages/HomePage';
 import About from './pages/AboutPage';
 
 function App() {
-const [count, setCount] = useState(0)
+// const [count, setCount] = useState(0)
 
 return (
   <>
