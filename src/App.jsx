533fd5bc204a2fd3b81493bbb57e9c6b414e43d1
SVemuri27 <sucharitha27102001@gmail.com>
2024-12-14 10:53:06 +0530

    nav1 commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         {/* <Route path="/react1" element={<React1 />} /> */}
       </Routes>
