99582671ba50889febab13b6c613ac4f939bee88
SVemuri27 <sucharitha27102001@gmail.com>
2024-12-14 11:25:35 +0530

    nav1 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Home from './pages/Home';
-import About from './pages/AboutPage';
-import Home from './pages/Home';
+import HomePage from './pages/Home';
+import About from './pages/About';
+// import React1 from './pages/React1';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/about" element={<Aboutpage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
         {/* <Route path="/react1" element={<React1 />} /> */}
       </Routes>
     </Router>
