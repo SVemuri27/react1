b7ff2b0c4ae875484aaa3f4d69a734fa48066ca0
SVemuri27 <sucharitha27102001@gmail.com>
2024-12-14 12:30:30 +0530

    nav2 commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/Homepage';
 import About from './pages/AboutPage';
-// import React1 from './pages/React1';
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/home" element={<HomePage />} />
         <Route path="/about" element={<About/>} />
-        {/* <Route path="/react1" element={<React1 />} /> */}
       </Routes>
     </Router>
   );
