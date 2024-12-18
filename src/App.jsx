358e6319b2ec0a330a33095c42eb46e7b094d2c5
SVemuri27 <sucharitha27102001@gmail.com>
2024-12-18 20:52:12 +0530

    git commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 // src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import User from './components/navbar/user';
 //import Home from './Home'; 
-import User from './User';
 
 const App = () => {
   return (
