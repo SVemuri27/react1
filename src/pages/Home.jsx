95b57dc01122bd7c5d935f714e2583e7bd2bf1a8
SVemuri27 <sucharitha27102001@gmail.com>
2024-12-18 15:05:30 +0530

    api commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
-// ./pages/HomePage.jsx
 import React from 'react';
+import DataFetchingComponent from '../components/DataFetchingComponent';
 
-function HomePage() {
-  return <div>Welcome to the Home Page</div>;
-}
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>sfscsd</p>
+      <DataFetchingComponent />
+    </div>
+  );
+};
 
 export default HomePage;
