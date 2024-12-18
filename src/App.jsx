617a60d89c65246e26d674385be3430a2f3f8e68
SVemuri27 <sucharitha27102001@gmail.com>
2024-12-18 19:00:18 +0530

    ap commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 // src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Homepage from './Home'; // Change to match the exported name
+import Homepage from './Homepages'; // Ensure this matches the exported name
 import User from './User '; // Ensure this import is correct
 
 const App = () => {
   return (
     <Router>
       <nav>
-        <Link to="/">Home</Link>
+        <Link to="/Homepages">Home</Link>
         <Link to="/user/1">User  1</Link>
         <Link to="/user/2">User  2</Link>
       </nav>
