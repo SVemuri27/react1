9e555a4ae917f0f1ca54d0c900247166d259f513
SVemuri27 <sucharitha27102001@gmail.com>
2024-12-18 13:46:32 +0530

    re commit

--- src/components/navbar/DataFetchingComponent.jsx
+++ src/components/navbar/DataFetchingComponent.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import React from 'react';
+
 const DataFetchingComponent = () => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
