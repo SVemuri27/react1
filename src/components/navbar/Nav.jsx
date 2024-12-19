bde435755677caa9629f059e0000e782863ebab5
SVemuri27 <sucharitha27102001@gmail.com>
2024-12-19 09:31:29 +0530

    git1 commit

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -1,18 +1,18 @@
-// Nav.jsx
-import React from 'react';
-// import {Link} form "react-router-dom"
-export const Nav = () => {
-  return (
-  <nav>
-  <ul>
-  <li>
-  <Link to="/about">About</Link>
-  </li>
-  <li><Link to="/home">home</Link>
-  </li>
-  </ul>
-  </nav>
-  );
-};
+// // Nav.jsx
+// import React from 'react';
+// // import {Link} form "react-router-dom"
+// export const Nav = () => {
+//   return (
+//   <nav>
+//   <ul>
+//   <li>
+//   <Link to="/about">About</Link>
+//   </li>
+//   <li><Link to="/home">home</Link>
+//   </li>
+//   </ul>
+//   </nav>
+//   );
+// };
 
-export default Nav;
+// export default Nav;
